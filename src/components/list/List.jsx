aef818bd772c723735b4690e29b917d393651429
minhazexo <minhazojy@gmail.com>
2024-09-15 01:25:30 +0600

    Updated List component to improve performance and fix rendering issues. modified:   src/components/list/List.jsx

--- src/components/list/List.jsx
+++ src/components/list/List.jsx
@@ -1,17 +1,12 @@
-import "./list.css"
-import Userinfo from "./userinfo/userinfo"
-import Chatlist from "./chatlist/chatlist"
+import "./list.css";
+import Userinfo from "./userinfo/Userinfo";
+import Chatlist from "./chatList/ChatList";
 const list = () => {
-
-    return (
-        <div className='list'>
-            <Userinfo/>
-            <Chatlist/>
-
-        
-        
-        
-        </div>
-    )   
-}
-export default list
+  return (
+    <div className="list">
+      <Userinfo />
+      <Chatlist />
+    </div>
+  );
+};
+export default list;
