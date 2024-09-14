4c7f578633860dcb1e33a9508e32e500b83b1eca
minhazexo <minhazojy@gmail.com>
2024-09-15 02:01:16 +0600

    do it

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0',             // Bind the server to 0.0.0.0 to expose it externally
+    port: process.env.PORT || 5173,  // Use PORT from environment variable or fallback to 5173
+  }
 })
